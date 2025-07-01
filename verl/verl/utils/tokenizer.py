0bafca19520f301e8679bf1c49b9dc7064190c09
yesiam-png <67275655+yesiam-png@users.noreply.github.com>
2025-06-30 23:54:38 -0700

    Update tokenizer.py

--- verl/verl/utils/tokenizer.py
+++ verl/verl/utils/tokenizer.py
@@ -52,7 +52,8 @@ def hf_tokenizer(name_or_path, correct_pad_token=True, correct_gemma2=True, **kw
         warnings.warn('Found gemma-2-2b-it tokenizer. Set eos_token and eos_token_id to <end_of_turn> and 107.')
         kwargs['eos_token'] = '<end_of_turn>'
         kwargs['eos_token_id'] = 107
-    tokenizer = AutoTokenizer.from_pretrained(name_or_path, **kwargs)
+    local_ = name_or_path.startswith('/')
+    tokenizer = AutoTokenizer.from_pretrained(name_or_path, local_files_only=local_, **kwargs)
     if correct_pad_token:
         set_pad_token_id(tokenizer)
     return tokenizer
