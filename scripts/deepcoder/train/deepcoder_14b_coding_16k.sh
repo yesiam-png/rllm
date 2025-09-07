def289b14d70b19c7d64533deee9dd01b963b021
root <root@bolt-7fpjsyjpji-axpdz326eq.bolt-pods.turi-bolt.svc.cluster.local>
2025-09-07 02:13:17 +0000

    rm

--- scripts/deepcoder/train/deepcoder_14b_coding_16k.sh
+++ scripts/deepcoder/train/deepcoder_14b_coding_16k.sh
@@ -65,7 +65,7 @@ python3 -m verl.trainer.main_ppo \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
     trainer.project_name='rlvr-sep6' \
-    trainer.experiment_name='rl-40-400-qwen-10warmup-5penalty-log-005lenpenalty-3sync_step2400-lcbformat' \
+    trainer.experiment_name='rl-40-400-qwen-10warmup-5penalty-log-005lenpenalty-3sync_step2400-areal' \
     +trainer.val_before_train=True \
     trainer.n_gpus_per_node=8 \
     trainer.nnodes=1 \
